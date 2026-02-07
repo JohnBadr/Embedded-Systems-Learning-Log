48d33ab9dde5cc93ebe83a9c58cb7d0427f6a3c7
John Badr <136370796+JohnBadr@users.noreply.github.com>
2026-02-06 22:04:14 -0500

    fixing my repo

--- P1 - Traffic Light/main.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-void setup();
-void loop();
--- P2 - Breathing LED/main.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-extern const int breathingLedPin;
-extern int brightness;
-extern int increments;
-
-void setup();
-void loop();
